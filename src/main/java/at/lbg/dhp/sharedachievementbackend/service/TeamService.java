8f13891fc91c9c96d5d99ca6e9564d4ee720a612
Sebastian <gruber.se@gmail.com>
2021-12-16 11:33:29 +0100

    Add files via upload

--- src/main/java/at/lbg/dhp/sharedachievementbackend/service/TeamService.java
+++ src/main/java/at/lbg/dhp/sharedachievementbackend/service/TeamService.java
@@ -3,6 +3,7 @@ package at.lbg.dhp.sharedachievementbackend.service;
 import at.lbg.dhp.sharedachievementbackend.data.dto.RelativeStepsDTO;
 import at.lbg.dhp.sharedachievementbackend.data.dto.SimpleStepCountDTO;
 import at.lbg.dhp.sharedachievementbackend.data.dto.TeamDTO;
+import at.lbg.dhp.sharedachievementbackend.data.dto.TeamMembersDTO;
 import at.lbg.dhp.sharedachievementbackend.data.models.Challenge;
 import at.lbg.dhp.sharedachievementbackend.data.models.Person;
 import at.lbg.dhp.sharedachievementbackend.data.models.StepCount;
@@ -134,5 +135,36 @@ public class TeamService {
         relativeStepsDTO.setRelativeSteps((100 * steps) / challenge.getSteps());
 
         return relativeStepsDTO;
+    }
+
+    public TeamMembersDTO getStepCountsOfTeamMembersToday(String name){
+        TeamMembersDTO teamMembersDTO = new TeamMembersDTO();
+        teamMembersDTO.setChallenge(new TeamMembersDTO().new Challenge());
+        teamMembersDTO.setPersons(new LinkedList<>());
+
+        Team team = teamRepository.findById(name).get();
+
+        teamMembersDTO.setName(team.getName());
+        teamMembersDTO.getChallenge().setName(team.getChallenge().getName());
+        teamMembersDTO.getChallenge().setSteps(team.getChallenge().getSteps());
+
+        List<Person> persons = team.getPersons();
+        for(Person person : persons){
+
+            TeamMembersDTO.Person newPerson = new TeamMembersDTO().new Person();
+            newPerson.setId(person.getId());
+            newPerson.setName(person.getName());
+
+            for(StepCount stepCount : person.getStepCounts()){
+                if (stepCount.getDay().equals(LocalDate.now())) {
+                    newPerson.setSteps((stepCount.getSteps()));
+                    newPerson.setRelativeNoOfSteps((100 * stepCount.getSteps()) / team.getChallenge().getSteps());
+                }
+            }
+
+            teamMembersDTO.getPersons().add(newPerson);
+        }
+
+        return teamMembersDTO;
     }
 }
