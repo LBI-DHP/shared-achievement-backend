e00163c406252c28c1b49db73ac2a8f48ab7d43b
Gmasek <masek.gergely98@gmail.com>
2026-09-15 10:59:22 +0200

    license plus headers added

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ Set Spring Boot Application Properties accordingly (src/main/resources/applicati
 Run application by maven (by default embedded in Tomcat).
 
 Check API on http://localhost:8080/swagger-ui/ .
+
+## License
+
+Copyright (c) 2021-2022 Ludwig Boltzmann Institute for Digital Health and Prevention.
+
+Licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+with the [Commons Clause](https://commonsclause.com/) License Condition v1.0.
+You may use, modify and redistribute this software, but you may not Sell it —
+that is, you may not provide to third parties, for a fee or other consideration,
+a product or service whose value derives entirely or substantially from this
+software's functionality.
+
+See [LICENSE](LICENSE) for the full terms.
--- src/main/java/at/lbg/dhp/sharedachievementbackend/data/dto/StepCountDTO.java
+++ src/main/java/at/lbg/dhp/sharedachievementbackend/data/dto/StepCountDTO.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2021-2022 Ludwig Boltzmann Institute for Digital Health and Prevention
+ *
+ * Licensed under the Apache License, Version 2.0 with the Commons Clause License
+ * Condition v1.0 (the "License"); you may not use this file except in compliance
+ * with the License. A copy of the License is distributed in the LICENSE file at
+ * the root of this repository; the Apache License is also available at
+ * http://www.apache.org/licenses/LICENSE-2.0 and the Commons Clause condition at
+ * https://commonsclause.com/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: LicenseRef-Apache-2.0-WITH-Commons-Clause
+ */
+
 package at.lbg.dhp.sharedachievementbackend.data.dto;
 
 import com.sun.istack.NotNull;
